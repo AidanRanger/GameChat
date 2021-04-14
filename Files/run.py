0f89eb720183692305c6340b220c2b6633d4283e
AidanRanger <21592@burnside.school.nz>
2021-04-15 09:21:02 +1200

    Created Games Class and a between class usergames

--- Files/run.py
+++ Files/run.py
@@ -14,14 +14,26 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 ###########################################################################################################
 #classes
-class user(db.Model):
+usergames = db.Table('usergames',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('games_id', db.Integer, db.ForeignKey('games.id'), primary_key=True)
+)
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable = False) 
-    password = db.Column(db.String(50), nullable = False)
+    username = db.Column(db.String(20), nullable=False) 
+    password = db.Column(db.String(50), nullable=False)
     def __repr__(self):
         return f'Username: {self.username}'
     def check_password(self, password):
         return self.password == password
+
+class Games(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    csgo = db.Column(db.String, nullable=False)
+    LoL = db.Column(db.String, nullable=False)
+    Apex = db.Column(db.String, nullable=False)
+    CoD = db.Column(db.String, nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
