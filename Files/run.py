a0d9b780e8ceb0b9d4b6492e01d190a161e3ae1c
AidanRanger <21592@burnside.school.nz>
2021-08-05 09:06:44 +1200

    Switched Login&Signup to info & newdata

--- Files/run.py
+++ Files/run.py
@@ -1,9 +1,11 @@
 from flask import Flask, g, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+'''
 from flask_login.mixins import UserMixin
 from flask_login.utils import login_required, logout_user
 from flask_login import LoginManager, login_user, current_user
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
+'''
 from datetime import datetime
 import sqlite3
 ###########################################################################################################
@@ -12,45 +14,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
 DATABASE = './DATABASES/db.sqlite3'
 ###########################################################################################################
-#classes
-usergames = db.Table('usergames',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('games_id', db.Integer, db.ForeignKey('games.id'), primary_key=True)
-)
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True) 
-    password = db.Column(db.String(50), nullable=False)
-    def __repr__(self):
-        return f'Username: {self.username}'
-    def check_password(self, password):
-        return self.password == password
-
-class Games(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    csgo = db.Column(db.String, nullable=False)
-    LoL = db.Column(db.String, nullable=False)
-    apex = db.Column(db.String, nullable=False)
-    CoD = db.Column(db.String, nullable=False)
-  
-class LoginForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    password = PasswordField('New Password', [
-        validators.DataRequired()
-    ])
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-###########################################################################################################
-#Logins
-login_manager.login_view = "users.login"
-###########################################################################################################
 #functions
 
 def get_db():
@@ -59,19 +25,19 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-###########################################################################################################
 #routes
 @app.route('/')
 def home():
     return render_template('home.html')
 
+'''
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        login_user(user)
-        flask.flash('Login Successful')
-    return flask.render_template('login.html', form=form)
+    #if form.validate_on_submit():
+    #    login_user(user)
+    #    flask.flash('Login Successful')
+    return render_template('login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -90,8 +56,16 @@ def logout():
     return 'You are now logged out!'
 
     return render_template('signup.html')
-@app.route('/games')
+'''
+@app.route('/NewData')
+def NewData():
+    return render_template('NewData.html')
 
+@app.route('/info')
+def Info():
+    return render_template('Info.html')
+
+@app.route('/games')
 def games():
     cursor = get_db().cursor()
     sql = 'SELECT games.csgo, games.LoL, games.Apex, games.CoD FROM usergames JOIN games ON usergames.games_id=games.id JOIN user ON usergames.user_id=user.id'
